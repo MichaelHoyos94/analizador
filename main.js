ee8d20b0ae5ff8930a1f14f2c1740c5520d02714
MichaelHoyos94 <maaguirreh@uqvirtual.edu.co>
2023-05-12 13:16:27 -0500

    Boton funcionando y corregidos errores

--- main.js
+++ main.js
@@ -11,9 +11,11 @@ const Tokens = {
     OPERADOR_RELACIONAL: `Operador relacional`,
     OPERADOR_LOGICO: `Operador logico`,
     OPERADOR_INCREMENTO: `Operador de incremento`,
-    ESPACIO_O_SALTO: 'Espacio o salto',
     OPERADOR_ASIGNACION: `Operador de asignacion`,
-    FIN_SENTENCIA: `Fin de sentencia`
+    FIN_SENTENCIA: `Fin de sentencia`,
+    APERTURA: 'Parentesis o llave de apertura',
+    CIERRE: `Parentesis o llave de cierre`,
+    SEPARADOR: `Separador`
 }
 /**
  * Representa un token, el analizador tendra una lista de estos.
@@ -22,8 +24,8 @@ class Token {
     constructor(tipo, valor, estado, ultimo) {
         this.tipo = tipo;
         this.valor = valor,
-            this.estado = estado,
-            this.ultimo = ultimo
+        this.estado = estado,
+        this.ultimo = ultimo
     };
 }
 /**
@@ -40,10 +42,8 @@ class analizador {
     analizar = () => {
         let i = 0;
         let token = null;
-        console.log(this.codigo);
-        console.log(this.codigo.length);
         while (i < this.codigo.length) {
-            if (this.codigo.charAt(i) != ` `) {
+            if (this.codigo.charAt(i) != ` ` && this.codigo.charAt(i) != `\n`) {
                 token = this.extraerSgteToken(i);
                 this.lista_tokens.push(token)
                 i = token.ultimo + 1;
@@ -52,207 +52,232 @@ class analizador {
             }
         }
         console.log(this.lista_tokens);
+        return this.lista_tokens;
     };
+    /**
+     * @author Michael Aguirre
+     * @description Extrae el token desde la posicion i, llama cada uno de los metodos para verificar el tipo de token.
+     * @param {*} i 
+     * @returns 
+     */
     extraerSgteToken = (i) => {
 
         let token = this.extraerNumero(i);
-        if (token) {
+        if (token)
             return token;
-        }
 
         token = this.extraerString(i);
-        if (token) {
+        if (token) 
             return token;
-        }
+
+        token = this.extraerAumDec(i);
+        if (token)
+            return token;
+
+        token = this.extraerOperAr(i);
+        if (token)
+            return token;
+
+        token = this.extraerComentario(i);
+        if (token) 
+            return token;
 
         token = this.extraerIdentificador(i);
-        if (token) {
+        if (token) 
             return token;
+
+        token = this.extraerAsignacion(i);
+        if (token)
+            return token;
+        
+        token = this.extraerLogico(i);
+        if (token)
+            return token;
+
+        token = this.extraerAperturaCierre(i);
+        if (token)
+            return token;
+        
+        token = this.extraerFin(i);
+        if (token)
+            return token;
+
+        token = this.extraerSeparador(i);
+        if (token)
+            return token;
+        
+        return new Token(Tokens.NO_RECONOCIDO, this.codigo.charAt(i), 'ERROR', i);
+    }
+    extraerString = (i) => {///an
+        if (this.codigo.charAt(i) == `/`) {
+            let pos = i;
+            i++;
+            while (i < this.codigo.length && this.codigo.charAt(i) !== `\n`) {
+                if (this.codigo.charAt(i) === `/`)
+                    return new Token(Tokens.CADENA_CARACTERES, this.codigo.substring(pos, i + 1), `OK`, i);
+                i++;
+            }
+            return new Token(Tokens.NO_RECONOCIDO, this.codigo.substring(pos, i), `CADENA SIN CERRAR`, i);
         }
-
-        /*token = this.quitarEspacio(i);
-        if (token) {
-            return token;
-            token = this.extraerAsignacion(i);
-            if (token)
-                return token;
-            token = this.extraerLogico(i);
-            if (token)
-                return token;
-            token = this.extraerFin(i);
-            if (token)
-                return token;*/
-            return new Token(Tokens.NO_RECONOCIDO, this.codigo.charAt(i), 'ERROR', i);
-        };
-
-
-        /*quitarEspacio(i){
-    
-            let caracter = this.codigo.charAt(i);
-    
-            if (caracter === ' ' || caracter === '\n') {
-                return new Token(Tokens.ESPACIO_O_SALTO, this.codigo.charAt(i), 'SALTO O ESPACIO', i);
+        return null;
+    };
+    extraerComentario = (i) => {//!
+        if (this.codigo.charAt(i) == `!`) {
+            let pos = i;
+            i++;
+            while (i < this.codigo.length && this.codigo.charAt(i) !== `\n`) {
+                if (this.codigo.charAt(i) === `!`)
+                    return new Token(Tokens.COMENTARIO_LINEA, this.codigo.substring(pos, i + 1), `OK`, i );
+                i++;
             }
-    
-            return null;
-    
-        }*/
-
-        extraerNumero = (i) => {
-            //Posicion Actual de la cadena
-            let caracter = this.codigo.charAt(i);
-
-            //Si posicion actual de la cadena es un numero
-            if (!isNaN(caracter)) {
-                let pos = i;
-                i++;
-
-                //Siga recorriendo mientras encuentre un numero o un punto
-                while (i < this.codigo.length && (!isNaN(caracter) || caracter === '.')) {
-                    i++;
-                    caracter = this.codigo.charAt(i);
-
-
-                }
-
-
-                //Evaluo si la subcadena es un numero entero
-                if (Number.isInteger(Number(this.codigo.substring(pos, i)))) {
-                    return new Token(Tokens.ENTERO, this.codigo.substring(pos, i), `OK`, i);
-
-                    //Evaluo si la subcadena es un numero decimal
-                } else if (Number.isFinite(Number(this.codigo.substring(pos, i)))) {
-                    return new Token(Tokens.DECIMAL, this.codigo.substring(pos, i), `OK`, i);
-
-                } else {
-                    return null;
-                }
-
-            }
-
-            return null;
-        };
-        extraerString = (i) => {
-            if (this.codigo.charAt(i) == `/`) {
-                let pos = i;
-                i++;
-                while (i < this.codigo.length && this.codigo.charAt(i) != `/`) {
-                    i++;
-                }
-                i++;
-                console.log(`Se extrajo un String`);
-                return new Token(Tokens.CADENA_CARACTERES, this.codigo.substring(pos, i), `OK`, i - 1);
-            }
-            return null;
-        };
-        extraerComentario = (i) => {
-            if (this.codigo.charAt(i) == `!`) {
-                let pos = i;
-                i++;
-                while (i < this.codigo.length && this.codigo.charAt(i) != `!`) {
-                    i++;
-                }
-                i++;
-                console.log(`Se extrajo un comentario`);
-                return new Token(Tokens.COMENTARIO_LINEA, this.codigo.substring(pos, i), `OK`, i - 1);
-            }
-            return null;
-        };
-        extraerAsignacion = (i) => {//=> i = 0;
-            if (this.codigo.charAt(i) == `=` && this.codigo.charAt(i + 1) == `>`)
-                return new Token(Tokens.OPERADOR_ASIGNACION, this.codigo.substring(i, i + 2), `OK`, i + 1);
-            return null;
-        };
-        extraerLogico = (i) => {
-            if (this.codigo.charAt(i) == `Y` || this.codigo.charAt(i) == `O` || this.codigo.charAt(i) == `N`)
-                return new Token(Tokens.OPERADOR_LOGICO, this.codigo.charAt(i), `OK`, i);
-            return null;
-        };
-        extraerFin = (i) => {
-            if (this.codigo.substring(i, this.codigo.length).startsWith(`FIN`))
-                return new Token(Tokens.FIN_SENTENCIA, `FIN`, `OK`, i + 2);
-            return null;
-        };
-
-
-        extraerIdentificador = (i) => {
-            //Valor con el que deben hacer match solo acepta si esta en mayuscula, empieza con $ y no termina con -
-            const pattern = /^\$[A-Z]+(-[A-Z]+)*$/;
-            //match si puso el identificador en minuscula
-            const patternError = /^\$[A-Z]+(-[A-Z]+)*$/i;
-            //martc para saber si es una letra
-            const patternLetra = /^[a-zA-Z]/;
-            let caracter = this.codigo.charAt(i);
-
-            if (caracter == "$") {
-                let pos = i;
+            return new Token(Tokens.NO_RECONOCIDO, this.codigo.substring(pos, i), `ERROR`, i);
+        }
+        return null;
+    };
+    extraerAsignacion = (i) => {
+        if (this.codigo.charAt(i) == `=` && this.codigo.charAt(i + 1) == `>`)
+            return new Token(Tokens.OPERADOR_ASIGNACION, this.codigo.substring(i, i + 2), `OK`, i + 1);
+        return null;
+    };
+    extraerLogico = (i) => {
+        if (this.codigo.charAt(i) == `Y` || this.codigo.charAt(i) == `O` || this.codigo.charAt(i) == `N`)
+            return new Token(Tokens.OPERADOR_LOGICO, this.codigo.charAt(i), `OK`, i);
+        return null;
+    };
+    extraerFin = (i) => {
+        if (this.codigo.substring(i, this.codigo.length).startsWith(`FIN`))
+            return new Token(Tokens.FIN_SENTENCIA, `FIN`, `OK`, i + 2);
+        return null;
+    };
+    extraerIdentificador = (i) => {
+        //Valor con el que deben hacer match solo acepta si esta en mayuscula, empieza con $ y no termina con -
+        const pattern = /^\$[A-Z]+(-[A-Z]+)*$/;
+        //match si puso el identificador en minuscula
+        const patternError = /^\$[A-Z]+(-[A-Z]+)*$/i;
+        //martc para saber si es una letra
+        const patternLetra = /^[a-zA-Z]/;
+        let caracter = this.codigo.charAt(i);
+        if (caracter == "$") {
+            let pos = i;
+            i++;
+            caracter = this.codigo.charAt(i);
+            //mientras sea una letra o un -
+            while (i < this.codigo.length && (patternLetra.test(caracter) || caracter === "-")) {
                 i++;
                 caracter = this.codigo.charAt(i);
 
-
-                //mientras sea una letra o un -
-                while (i < this.codigo.length && (patternLetra.test(caracter) || caracter === "-")) {
-                    i++;
-                    caracter = this.codigo.charAt(i);
-
-                }
-                //si hace match retorne el token tipo identificador
-                if (pattern.test(this.codigo.substring(pos, i))) {
-                    console.log("Se extrajo un identificador.");
-                    return new Token(Tokens.IDENTIFICADOR, this.codigo.substring(pos, i), "OK", i);
-                } else if (caracter === "-") { // sino retorne un tipo token no reconocido con un error
-                    return new Token(Tokens.NO_RECONOCIDO, this.codigo.substring(pos, i), "ERROR-IDENTIFICADOR SIN CERRAR", i);
-                } else if (patternError.test(this.codigo.substring(pos, i))) { // sino retorne un tipo token no reconocido con un error
-                    return new Token(Tokens.NO_RECONOCIDO, this.codigo.substring(pos, i), "ERROR-IDENTIFICADOR EN MINUSCULAS", i);
-                }
             }
-            return null;
-        };
-
-        extraerString = (i) => {
-
-            //Valor con el que deben hacer match solo acepta si empieza y termina con /
-            const pattern = /^\/.*\/$/;
-            let caracter = this.codigo.charAt(i);
-
-            if (caracter === '/') {
-                let pos = i;
+            //si hace match retorne el token tipo identificador
+            if (pattern.test(this.codigo.substring(pos, i))) {
+                console.log("Se extrajo un identificador.");
+                return new Token(Tokens.IDENTIFICADOR, this.codigo.substring(pos, i), "OK", i);
+            } else if (caracter === "-") { // sino retorne un tipo token no reconocido con un error
+                return new Token(Tokens.NO_RECONOCIDO, this.codigo.substring(pos, i), "ERROR-IDENTIFICADOR SIN CERRAR", i);
+            } else if (patternError.test(this.codigo.substring(pos, i))) { // sino retorne un tipo token no reconocido con un error
+                return new Token(Tokens.NO_RECONOCIDO, this.codigo.substring(pos, i), "ERROR-IDENTIFICADOR EN MINUSCULAS", i);
+            }
+        }
+        return null;
+    };
+    /*
+    extraerString = (i) => {
+        //Valor con el que deben hacer match solo acepta si empieza y termina con /
+        const pattern = /^\/.*\/$/;
+        let caracter = this.codigo.charAt(i);
+        if (caracter === '/') {
+            let pos = i;
+            i++;
+            //mientras el caracter sea diferente a un salto de linea
+            while (i < this.codigo.length && caracter !== '\n') {
                 i++;
-
-                //mientras el caracter sea diferente a un salto de linea
-                while (i < this.codigo.length && caracter !== '\n') {
+                caracter = this.codigo.charAt(i);
+                //Si encuientra otro / termine el ciclo que hemos terminado de encontrar nuestra cadena
+                if (caracter === '/') {
                     i++;
                     caracter = this.codigo.charAt(i);
-
-                    //Si encuientra otro / termine el ciclo que hemos terminado de encontrar nuestra cadena
-                    if (caracter === '/') {
-                        i++;
-                        caracter = this.codigo.charAt(i);
-
-
-                        break;
-                    }
-
-                }
-
-                //Si la cadena hace martch con el pattern retorne el token tipo cadena de caracteres
-                if (pattern.test(this.codigo.substring(pos, i))) {
-                    return new Token(Tokens.CADENA_CARACTERES, this.codigo.substring(pos, i), `OK`, i);
+                    break;
                 }
             }
+            //Si la cadena hace martch con el pattern retorne el token tipo cadena de caracteres
+            if (pattern.test(this.codigo.substring(pos, i))) {
+                return new Token(Tokens.CADENA_CARACTERES, this.codigo.substring(pos, i), `OK`, i);
+            }
+        }
+        return null;
+    };*/
+    extraerNumero = (i) => {
+        //Posicion Actual de la cadena
+        let caracter = this.codigo.charAt(i);
+        //Si posicion actual de la cadena es un numero
+        if (!isNaN(caracter)) {
+            let pos = i;
+            i++;
 
-            return null;
-        };
+            //Siga recorriendo mientras encuentre un numero o un punto
+            while (i < this.codigo.length && (!isNaN(caracter) || caracter === '.')) {
+                i++;
+                caracter = this.codigo.charAt(i);
+            }
+            //Evaluo si la subcadena es un numero entero
+            if (Number.isInteger(Number(this.codigo.substring(pos, i)))) {
+                return new Token(Tokens.ENTERO, this.codigo.substring(pos, i), `OK`, i);
+                //Evaluo si la subcadena es un numero decimal
+            } else if (Number.isFinite(Number(this.codigo.substring(pos, i)))) {
+                return new Token(Tokens.DECIMAL, this.codigo.substring(pos, i), `OK`, i);
+            } else {
+                return null;
+            }
+        }
+        return null;
+    };
+    extraerAperturaCierre = (i) =>{
+        let char = this.codigo.charAt(i);
+        if (char === `(` || char === `[`)
+            return new Token(Tokens.APERTURA, char, 'OK', i);
+        else if (char === `)` || char === `]`)
+            return new Token(Tokens.CIERRE, char, `OK`, i);
+        return null;
+    };
+    extraerSeparador = (i) =>{
+        let char = this.codigo.charAt(i)
+        if (char === `,`)
+            return new Token(Tokens.SEPARADOR, char, `OK`, i);
+        return null;
+    };
+    extraerAumDec = (i) =>{ 
+        if (this.codigo.charAt(i) === `+` && this.codigo.charAt(i + 1) === `+`)
+            return new Token(Tokens.OPERADOR_INCREMENTO, this.codigo.substring(i, i + 2), `OK`, i + 1);
+        if (this.codigo.charAt(i) === `-` && this.codigo.charAt(i + 1) === `-`)
+            return new Token(Tokens.OPERADOR_INCREMENTO, this.codigo.substring(i, i + 2), `OK`, i + 1);
+        return null;
+    };
+    extraerOperAr = (i) =>{
+        let char = this.codigo.charAt(i);
+        if (char === `+` || char === `-` || char === `*`)
+            return new Token(Tokens.OPERADOR_ARITMETICO, char, `OK`, i)
+        return null;
+    };
+}
 
+const codigo = document.getElementById(`txt-codigo`);
+const btn = document.getElementById("btn_analizar");
+const res = document.getElementById("res-list");
+    
+btn.addEventListener('click', function() {
+    const codigoFuente = codigo.value;
+    const a = new analizador(codigoFuente, []);
+    const tokens_analizados = a.analizar();
+    let elementos = ``;
+    tokens_analizados.forEach(element => {
+        if(element.estado === `OK`)
+            elementos += `<li class="okToken">Tipo: ${element.tipo} | Valor: ${element.valor} | Estado: ${element.estado}</li>\n`
+        else
+            elementos += `<li class="wrongToken">Tipo: ${element.tipo} | Valor: ${element.valor} | Estado: ${element.estado}</li>\n`
+    });
+    res.innerHTML = elementos;
+});
 
-    }
-
-    const codigo1 = "$id =>2 @@@ /CADENA/!COMEN!YONFIN";
-    const a = new analizador(codigo1, []);
-    a.analizar();
-    let cad = `aaa`;
-    let ocad = cad.substring(0, 3);
-    console.log(ocad);
-
-
-
+/*
+const codigo1 = "$ID =>2 @@@++*-+ ,,[[[(/CADENA/!COMEN!YONFIN)";
+const a = new analizador(codigo1, []);
+a.analizar();*/
